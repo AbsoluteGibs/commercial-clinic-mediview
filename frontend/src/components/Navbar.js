2cfb889204d574dedaf2a51b6b8bc2b92efea8e7
Zhu Zhanyan <program.nom@gmail.com>
2021-06-17 22:04:52 +0800

    refactor(navbar): Refactor Navbar from class to functional component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../navbar.css'
 import {Link} from 'react-router-dom'
 
@@ -22,44 +22,32 @@ function Navitem(props) {
 /**
  * Represents a Navbar that allows the user to navigate across the page
  */
-class Navbar extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            // whether to show the navbar in mobile screens <= 800px
-            showNavbar: true,
-        }
-        // explicitly bind 'this' in order to use 'this' in callback
-        this.toggle = this.toggle.bind(this);
-    }
+function Navbar(_props) {
+    // showNavbar: whether to show the navbar in mobile screens <= 800px
+    const [navbarShown, setNavbarShown] = useState(true);
 
-    /**
-     * Toggles the Navbar between showing and hiding in mobile screens < 800px.
-     * Always shows the Navbar if on a viewport larger than 800px.
-     */
-    toggle() {
-        this.setState({
-            showNavbar: (window.innerHeight > 800) ? true : !this.state.showNavbar
-        })
-    }
-
-    render() {
-        return (
-            <div>
-                <nav id='navbar'>
-                    {/* see navbar.css to understand how navbar is toggled by checkbox */}
-                    <input type='checkbox' id='responsive-navbar' onClick={this.toggle} checked={this.state.showNavbar}/><label></label>
-                    <ul>
-                        <Navitem to='/articles' onClick={this.toggle}>Articles</Navitem>
-                        <Navitem to='/about-us' onClick={this.toggle}>About Us</Navitem>
-                        <Navitem to='/contact-us' onClick={this.toggle}>Contact Us</Navitem>
-                        <Navitem to='/covid-services' onClick={this.toggle}>COVID-19 Services</Navitem>
-                        <Navitem to='/health-screening' onClick={this.toggle}>Health Screening Packages</Navitem>
-                    </ul>
-                </nav>
-            </div>
+    // toggle the Navbar between showing and hiding in mobile screens < 800px.
+    const toggleNavbar = () => {
+        setNavbarShown(
+            // always shows the Navbar if on a viewport larger than 800px.
+            (window.innerHeight > 800) ? true : !navbarShown
         );
     }
+    return (
+        <div>
+            <nav id='navbar'>
+                {/* see navbar.css to understand how navbar is toggled by checkbox */}
+                <input type='checkbox' id='responsive-navbar' onClick={toggleNavbar} checked={navbarShown}/><label></label>
+                <ul>
+                    <Navitem to='/articles' onClick={toggleNavbar}>Articles</Navitem>
+                    <Navitem to='/about-us' onClick={toggleNavbar}>About Us</Navitem>
+                    <Navitem to='/contact-us' onClick={toggleNavbar}>Contact Us</Navitem>
+                    <Navitem to='/covid-services' onClick={toggleNavbar}>COVID-19 Services</Navitem>
+                    <Navitem to='/health-screening' onClick={toggleNavbar}>Health Screening Packages</Navitem>
+                </ul>
+            </nav>
+        </div>
+    );
 }
 
 export default Navbar;
