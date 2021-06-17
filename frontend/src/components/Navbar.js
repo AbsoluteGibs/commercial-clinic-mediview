cb979d47f62cef8862327fc58a1d4a1f0dac83a5
Zhu Zhanyan <program.nom@gmail.com>
2021-06-17 19:35:23 +0800

    fix(navbar): Fix responsive navbar not hiding on page navigation.
    * Replaces navigates items with Navitem component.
    * Refactors Navbar component into a class component to store navbar
      state.
    * Add toggle() method to toggle navbar show/hide, hook up checkbox to
      show / hide navbar.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,27 +2,64 @@ import React from 'react'
 import '../navbar.css'
 import {Link} from 'react-router-dom'
 
-function Navbar(){
+/**
+ * Defines a Navigation Item shown in a Navbar that navigates to the given URL when clicked.
+ *
+ * @param props
+ * @param {string} props.to target URL to navigate to.
+ * @param {function} props.onClick callback called on click event.
+ * @param {function} children child components to render in navbar.
+ *
+ */
+function Navitem(props) {
+    return (
+        <li class="nav-item">
+            <Link to={props.to} onClick={props.onClick}>{props.children}</Link>
+        </li>
+    );
+}
 
-    function updatenav()
-    {
-
+/**
+ * Represents a Navbar that allows the user to navigate across the page
+ */
+class Navbar extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            // whether to show the navbar in mobile screens <= 800px
+            showNavbar: true,
+        }
+        // explicitly bind 'this' in order to use 'this' in callback
+        this.toggle = this.toggle.bind(this);
     }
 
-    return(
-        <div>
-            <nav id='navbar'>
-                <input type='checkbox' id='responsive-navbar'/><label></label>
-                <ul>
-                    <li><Link to='/articles' onClick={updatenav}>Articles</Link></li>
-                    <li><Link to='/about-us' onClick={updatenav}>About Us</Link></li>
-                    <li><Link to='/contact-us' onClick={updatenav}>Contact Us</Link></li>
-                    <li><Link to='/covid-services' onClick={updatenav}>COVID-19 Services</Link></li>
-                    <li><Link to='/health-screening' onClick={updatenav}>Health Screening Packages</Link></li>
-                </ul>
-            </nav>
-        </div>
-    );
+    /**
+     * Toggles the Navbar between showing and hiding in mobile screens < 800px.
+     * Always shows the Navbar if on a viewport larger than 800px.
+     */
+    toggle() {
+        this.setState({
+            showNavbar: (window.innerHeight > 800) ? true : !this.state.showNavbar
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <nav id='navbar'>
+                    {/* see navbar.css to understand how navbar is toggled by checkbox */}
+                    <input type='checkbox' id='responsive-navbar' onClick={this.toggle} checked={this.state.showNavbar}/><label></label>
+                    <ul>
+                        <Navitem to='/articles' onClick={this.toggle}>Articles</Navitem>
+                        <Navitem to='/about-us' onClick={this.toggle}>About Us</Navitem>
+                        <Navitem to='/contact-us' onClick={this.toggle}>Contact Us</Navitem>
+                        <Navitem to='/covid-services' onClick={this.toggle}>COVID-19 Services</Navitem>
+                        <Navitem to='/health-screening' onClick={this.toggle}>Health Screening Packages</Navitem>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
 }
 
 export default Navbar;
