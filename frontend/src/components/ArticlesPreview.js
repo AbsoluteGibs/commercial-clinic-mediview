2ef6923e142c4aeec81ae285404bc39d7b3fcb14
Kelvin <zhpskelvinchew@gmail.com>
2021-06-19 17:21:56 +0800

    fixed articlepreview component css issues

--- frontend/src/components/ArticlesPreview.js
+++ frontend/src/components/ArticlesPreview.js
@@ -4,17 +4,19 @@ import {Link} from 'react-router-dom'
 function ArticlesPreview({article})
 {
     return(
-        <div className="article_panel">
+        <div>
             <Link className='no_text_decor' to={`/articles/${article.name}`}>
-                <div>
-                    <div style={{maxWidth: '200px', display: 'inline-block', paddingRight: '20px'}}>
-                        <img style={{borderRadius: '12px', maxWidth: '100%', maxHeight: '100%'}} src={require(`../images/${article.thumbnail}`)} alt={article.name}/>
-                        {console.log(article.thumbnail)}
-                    </div>
-                    <div style={{maxWidth: '800px', display: 'inline-block', margin: '0'}}>
-                        <h3 style={{paddingBottom: '30px', paddingTop: '30px'}}>{article.title}</h3>
-                        Published Date: {article.dateOfPublish}<br/>
-                        {article.shortDesc}
+                <div className="article_panel">
+                    <div>
+                        <div style={{maxWidth: '200px', display: 'inline-block', paddingRight: '20px'}}>
+                            <img style={{borderRadius: '12px', maxWidth: '100%', maxHeight: '100%'}} src={require(`../images/${article.thumbnail}`)} alt={article.name}/>
+                            {console.log(article.thumbnail)}
+                        </div>
+                        <div style={{maxWidth: '800px', display: 'inline-block', margin: '0'}}>
+                            <h3 style={{paddingBottom: '30px', paddingTop: '30px'}}>{article.title}</h3>
+                            Published Date: {article.dateOfPublish}<br/>
+                            {article.shortDesc}
+                        </div>
                     </div>
                 </div>
             </Link>
