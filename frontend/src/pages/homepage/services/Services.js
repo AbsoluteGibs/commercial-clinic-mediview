1bfe8b50666f0f1b8cb3d738c7d9fcc7da4797b1
Zhu Zhanyan <program.nom@gmail.com>
2021-06-20 21:27:05 +0800

    feat(homepage): Add services page section of homepage

---
+++ frontend/src/pages/homepage/services/Services.js
@@ -0,0 +1,57 @@
+/*
+ * commercial-clinic-mediview
+ * Home Page: Services section
+*/
+
+
+import React from 'react'
+import Card from '../../../components/card/Card'
+import ScreeningBackground from '../../../images/home_page_services_screening_bg@1x.png'
+import TestingBackground from '../../../images/home_page_services_testing_bg@1x.png'
+import VaccineBackground from '../../../images/home_page_services_vaccine_bg@1x.png'
+
+import './Services.css'
+
+/**
+ * id assigned to the rendered {@link Service} component
+ * @constant {string} 
+*/
+export const SERVICE_SECTION_ID = "services";
+
+/**
+ * ServicesSection component renders the services section on the HomePage
+*/
+export default function ServicesSection() {
+    return (
+        <section id={SERVICE_SECTION_ID}>
+            <h1>Our Services</h1>
+            
+            <div className="card-deck">
+                <Card 
+                    title="Health Screening"
+                    backgroundSrc={ScreeningBackground}
+                    ctaTitle="Learn More"
+                    ctaTo="/health-screening">
+                    <p> Health screening is essential for early detection of diseases and conditions.  </p>
+                    <p> Mediview provides affordable & tailored health screening packages. </p>
+                </Card>
+                <Card 
+                    title="COVID-19 Testing"
+                    backgroundSrc={TestingBackground}
+                    ctaTitle="Learn More"
+                    ctaTo="/covid-services">
+                    <p>Pre-event testing is required for large scale events such as trade shows and weddings.</p>
+                    <p>Mediview provides ART tests for events and PCR tests individuals going overseas.</p>
+                </Card>
+                <Card 
+                    title="Sinopac Vaccine"
+                    backgroundSrc={VaccineBackground}
+                    ctaTitle="Learn More"
+                    ctaTo="/covid-services">
+                    <p>Protect yourself and your loved ones from COVID-19 by getting vaccinated.</p>
+                    <p>Mediview provides COVID-19 vaccination via the Sinopac Vaccine.</p>
+                </Card>
+            </div>
+        </section>
+    )
+}
