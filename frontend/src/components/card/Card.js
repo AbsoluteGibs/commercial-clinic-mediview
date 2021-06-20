9afe1b1f5e64e386f39f89999c8b5062936a01a0
Zhu Zhanyan <program.nom@gmail.com>
2021-06-20 19:11:44 +0800

    feat(components): Add card react component to render a card UI element.

---
+++ frontend/src/components/card/Card.js
@@ -0,0 +1,36 @@
+/*
+ * commercial-clinic-mediview
+ * Card UI Component
+*/
+
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+import './Card.css'
+import '../Button.css'
+
+/**
+ * Renders a Card IE element with header, description and call to action.
+ *
+ * @param {Object} props Card component props.
+ * @param {string} props.title Title to show in the cards header.
+ * @param {string} props.backgroundSrc URL to an image to show as a background in the header.
+ * @param {string} props.children Child components / elements to show in the body of the card.
+ * @param {string} props.ctaTitle Title of the call to action button.
+ * @param {string} props.ctaTo URL to navigate to if the call to action button is clicked.
+*/
+export default function Card(props) {
+    return (
+        <div className="card">
+            <div className="header">
+                <img src={props.backgroundSrc} alt="background" />
+                <h2 className="title">{props.title}</h2>
+            </div>
+            <div className="body">
+                {props.children}
+            </div>
+            <Link className="button" to={props.ctaTo}>{props.ctaTitle}</Link>
+        </div>
+    )
+}
+
