7bc0fdd9b2d7a3249173e37b92e0761a123794de
Kelvin <zhpskelvinchew@gmail.com>
2021-06-21 19:58:09 +0800

    merging homepage+footer completion with sinovac page with routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,12 +10,9 @@ import Articles from './components/ArticlesContent'
 import ContactUs from './components/ContactUs'
 import CovidServices from './components/CovidServices'
 import HealthScreening from './components/HealthScreening'
-<<<<<<< HEAD
 import SinovacAdvisory from './components/SinovacAdvisory'
-=======
 import HomePage from './pages/homepage/HomePage'
 import Footer from './components/footer/Footer'
->>>>>>> c11caef1284cfdb006ee81c9ff2db56381cfb17a
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
@@ -24,7 +21,6 @@ function App() {
         <div id="root">
             <Router>
                 <header>
-                    {/* might wanna incorporate logo into navbar */}
                     <div className="logo-container-top">
                         <div className="logo-container">
                             <Logo/>
@@ -42,6 +38,7 @@ function App() {
                         <Route path="/contact-us" exact component={ContactUs}/>
                         <Route path="/covid-services" exact component={CovidServices}/>
                         <Route path="/health-screening" exact component={HealthScreening}/>
+                        <Route path="/sinovac-advisory" exact component={SinovacAdvisory}/>
                         <Route path="/" exact component={HomePage}/>
                     </Switch>
                 </main>
@@ -49,28 +46,7 @@ function App() {
                 <Footer />
             </Router>
         </div>
-<<<<<<< HEAD
-        <Navbar/>
-        <header className="App-header">
-          <div>
-            {/* Where content is generated depending on page clicked, aka website body */}
-            <Switch>
-              <Route path="/articles" exact component={Articles}/>
-              <Route path="/articles/:fullArticle" component={Article}/>
-              <Route path="/about-us" exact component={About}/>
-              <Route path="/contact-us" exact component={ContactUs}/>
-              <Route path="/covid-services" exact component={CovidServices}/>
-              <Route path="/health-screening" exact component={HealthScreening}/>
-              <Route path="/sinovac-advisory" exact component={SinovacAdvisory}/>
-            </Switch>
-          </div>
-        </header>
-      </Router>
-    </div>
-  );
-=======
     );
->>>>>>> c11caef1284cfdb006ee81c9ff2db56381cfb17a
 }
 
 export default App
