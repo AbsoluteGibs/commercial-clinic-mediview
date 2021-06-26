693a86d79c5efe381363e9e432d70da93d8eb2e7
Kelvin <zhpskelvinchew@gmail.com>
2021-06-26 14:13:25 +0800

    allow linked form to open in new tab

--- frontend/src/components/VaccineFloatingButton.js
+++ frontend/src/components/VaccineFloatingButton.js
@@ -3,7 +3,7 @@ import React from 'react'
 function VaccineFloatingButton() {
     return (
         <div className="flex float">
-            <a href="https://forms.gle/9MBvJZyvdc4Uck3A8">Register for Sinovac Vaccine (SINOVAC 科兴) here</a>
+            <a href="https://forms.gle/9MBvJZyvdc4Uck3A8" target="_blank" rel="noopener noreferrer">Register for Sinovac Vaccine (SINOVAC 科兴) here</a>
         </div>
     )
 }
